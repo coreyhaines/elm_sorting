0951424a5a6b078d9a17b7ed0e017f1db72bc0bd
Corey Haines <coreyhaines@gmail.com>
2016-12-30 20:11:43 -0600

    base case for empty array

---
+++ src/BinarySearch.elm
@@ -0,0 +1,6 @@
+module BinarySearch exposing (search)
+
+
+search : List a -> a -> Int
+search array key =
+    -1
--- tests/BinarySearchTests.elm
+++ tests/BinarySearchTests.elm
@@ -1,5 +1,6 @@
 module BinarySearchTests exposing (..)
 
+import BinarySearch
 import Test exposing (..)
 import Expect
 
@@ -7,4 +8,11 @@ import Expect
 all : Test
 all =
     describe "Binary Search"
-        []
+        [ searchTests ]
+
+
+searchTests : Test
+searchTests =
+    describe "Searching Tests"
+        [ test "Searching an empty array" <| \() -> Expect.equal -1 <| BinarySearch.search [] 1
+        ]
