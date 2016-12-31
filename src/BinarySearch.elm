6d591f21a8aa169bc5a06f9a18c5d501c0e14f6f
Corey Haines <coreyhaines@gmail.com>
2016-12-30 20:48:07 -0600

    Binary Search working

--- src/BinarySearch.elm
+++ src/BinarySearch.elm
@@ -1,5 +1,7 @@
 module BinarySearch exposing (search)
 
+import List.Extra as List
+
 
 search : List Int -> Int -> Int
 search items item =
@@ -7,8 +9,41 @@ search items item =
         [] ->
             -1
 
-        x :: xs ->
-            if x == item then
-                0
-            else
-                -1
+        _ ->
+            let
+                midIndex =
+                    (List.length items) // 2
+
+                left =
+                    List.take midIndex items
+
+                right =
+                    List.drop (midIndex + 1) items
+
+                midValue =
+                    List.getAt midIndex items
+
+                isMid value =
+                    if value == item then
+                        midIndex
+                    else if value > item then
+                        search left item
+                    else
+                        let
+                            foundInRight =
+                                search right item
+                        in
+                            if foundInRight == -1 then
+                                -1
+                            else
+                                foundInRight + midIndex + 1
+            in
+                Maybe.map isMid midValue
+                    |> Maybe.withDefault -1
+
+
+
+-- get a midpoint (floor)
+-- does item == items[midpoint] if so return midpoint
+-- if item < items[midpoint] then search items low..mid - 1
+-- if item > items[midpoint ] then search items mid + 1 - high
--- tests/BinarySearchTests.elm
+++ tests/BinarySearchTests.elm
@@ -26,13 +26,20 @@ notFoundTests =
     describe "Not Found"
         [ testNotFound "Searching an empty items" [] 1
         , testNotFound "Searching a one-element items" [ 0 ] 1
+        , testNotFound "Searching a two-element array" [ 0, 1 ] 3
+        , testNotFound "Four element" [ 1, 2, 3, 10 ] 5
         ]
 
 
 foundTests : Test
 foundTests =
     describe "Found"
-        [ testFound "One element items" [ 1 ] 1 0 ]
+        [ testFound "One element items" [ 1 ] 1 0
+        , testFound "Two element first" [ 1, 2 ] 1 0
+        , testFound "Two element second" [ 1, 2 ] 2 1
+        , testFound "Four element last" [ 1, 2, 3, 10 ] 10 3
+        , testFound "Four element last" [ 1, 2, 3, 10, 11 ] 10 3
+        ]
 
 
 searchTests : Test
